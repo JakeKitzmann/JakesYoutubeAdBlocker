6ea60e8ad848d4b062eaf4b4ef4150c2cd79dc88
JakeKitzmann <97077033+JakeKitzmann@users.noreply.github.com>
2025-07-10 07:20:26 -0500

    update readme

--- background.js
+++ background.js
@@ -12,9 +12,9 @@ chrome.action.onClicked.addListener(async (tab) => {
 			16: "icons/logo_16x16.png",
 			32: "icons/logo_32x32.png",
 			48: "icons/logo_48x48.png"
-		}
+	
 	});
 
-	// title
+    // title
 	chrome.action.setTitle({ tabId: tab.id, title: "AdBlocker Running" });
 });
--- readme.md
+++ readme.md
@@ -1,7 +1,4 @@
-I'm too lazy to make a readme. I literally made a program that presses
-skip for me. You think I'm going to write this shit?
-
-Kai's bitch ass wants directions
+Setup:
 
 go to chrome://extensions/
 
